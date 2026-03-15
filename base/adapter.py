d4460e492d48e21f29740c9ef23ec7fc9fc163df
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 13:16:33 +0600

    Simplify get_connect_redirect_url method
    Refactor redirect URL logic for social account connections.

--- base/adapter.py
+++ base/adapter.py
@@ -2,10 +2,9 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from . models import UserProfile
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_login_redirect_url(self, request):
-        if request.user.socialaccount_set.exists() and request.user.last_login is None:
-            return resolve_url("/")
-        return resolve_url("/")
+
+    def get_connect_redirect_url(self, request, socialaccount):
+        return "/"
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
