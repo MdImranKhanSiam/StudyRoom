fbecc493aca5c6e3e856c401dc90b4b419da649a
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 18:06:55 +0600

    bug

--- studybud/settings.py
+++ studybud/settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+   
 ]
 
 
