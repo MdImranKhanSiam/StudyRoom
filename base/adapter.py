e43ae8ab4dd60f9d01c40802f1d9a75786b3f6fa
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 17:47:17 +0600

    Bug

--- base/adapter.py
+++ base/adapter.py
@@ -2,21 +2,21 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.shortcuts import redirect
 from . models import UserProfile
 
-class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+# class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
 
-    def save_user(self, request, sociallogin, form=None):
-        user = super().save_user(request, sociallogin, form)
-        data = sociallogin.account.extra_data
+#     def save_user(self, request, sociallogin, form=None):
+#         user = super().save_user(request, sociallogin, form)
+#         data = sociallogin.account.extra_data
 
-        name = data.get('name')
-        avatar = data.get('picture')
+#         name = data.get('name')
+#         avatar = data.get('picture')
 
-        UserProfile.objects.get_or_create(
-            user=user,
-            defaults={
-                'display_name': name,
-                'avatar': avatar,
-            }
-        )
+#         UserProfile.objects.get_or_create(
+#             user=user,
+#             defaults={
+#                 'display_name': name,
+#                 'avatar': avatar,
+#             }
+#         )
 
-        return user
+#         return user
