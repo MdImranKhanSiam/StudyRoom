22e090b8a48865d8c264cfe376f61318560419d4
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 13:31:16 +0600

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -230,5 +230,5 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
