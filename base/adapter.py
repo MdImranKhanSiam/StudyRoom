12771b447c1f2ba1fc437991a6812663b11848e5
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 14:01:46 +0600

    Bug fix

--- base/adapter.py
+++ base/adapter.py
@@ -1,10 +1,14 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.shortcuts import redirect
 from . models import UserProfile
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-
-    def get_login_redirect_url(self, request):
-        return "/"
+    def authentication_error(self, request, provider_id, error=None, exception=None, extra_context=None):
+        """
+        Called when a social authentication attempt fails.
+        We'll redirect the user to the homepage.
+        """
+        return redirect('/')
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
