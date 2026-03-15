be980fd7808aaa7e7145df0b916ef50dea5291bb
Imran Khan <mdimrankhansiam@gmail.com>
2026-03-15 13:04:51 +0600

    Add custom login redirect logic to adapter

--- base/adapter.py
+++ base/adapter.py
@@ -2,6 +2,10 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from . models import UserProfile
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def get_login_redirect_url(self, request):
+        if request.user.socialaccount_set.exists() and request.user.last_login is None:
+            return resolve_url("/")
+        return resolve_url("/")
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
